0143e3fade302ca2e22fe18e4cd1d5510a375a0f
vivoil <roberto@vivoil.com>
2016-11-12 00:03:06 +0100

    add webpack

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
@@ -16,3 +27,6 @@ app.listen(port, function(err) {
         open('http://localhost:' + port);
     }
 })
+
+
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+import numeral from'numeral';
+
+const couseValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log(`I would pay ${couseValue} for this awesome course!`);
+
+
+
